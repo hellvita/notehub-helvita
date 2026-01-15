4ad2282971e36e661809b8e298e2a12aec9c74d2
hellvita <seredaolga03@gmail.com>
2026-01-16 00:41:43 +0200

    add styles for the logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,10 +2,26 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-black-900">
-      <Link href="/" aria-label="Home">
-        NoteHub
-      </Link>
+    <header className="bg-black-900 ">
+      <div className="px-5 tablet-big:px-10 flex justify-between items-center">
+        <Link
+          href="/"
+          aria-label="Home"
+          className="mr-4 mobile:text-s24 font-bold py-2 hover:text-pink-400/60 transition-colors duration-300"
+        >
+          NoteHub
+        </Link>
+        <nav aria-label="Main Navigation">
+          <ul className="flex gap-x-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Notes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
---
+++ components/Header/NavMobile/NavMobile.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import { GiHamburgerMenu as IconMenu } from "react-icons/gi";
+import { FaArrowLeftLong as IconBack } from "react-icons/fa6";
+
+export default function NavMobile() {}

4e7575748d03b33ae97c4dbc7108b98acfa14a1a
hellvita <seredaolga03@gmail.com>
2026-01-15 20:18:41 +0200

    add the Footer component for tablet, add font-sizes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black-900 py-4 text-center text-white-400 leading-8">
-      <div className="my-0 mx-auto w-80 max-w-full flex flex-col gap-y-5">
+      <div className="my-0 mx-auto min-w-80 flex flex-col gap-y-5 px-5 tablet-big:px-10">
         <p>© 2025 NoteHub. All rights reserved.</p>
         <div className="flex flex-col gap-y-5">
           <p>Developer: Olha Sereda</p>

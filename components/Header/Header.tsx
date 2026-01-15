404f2ddab6648f34d0fe90256d41e29edcc5cbfb
hellvita <seredaolga03@gmail.com>
2026-01-15 20:53:56 +0200

    add the Home button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <header className="bg-black-900">Header</header>;
+  return (
+    <header className="bg-black-900">
+      <Link href="/" aria-label="Home">
+        NoteHub
+      </Link>
+    </header>
+  );
 }

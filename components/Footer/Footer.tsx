672b7af7f25467c24e9c24967af4c09b71d290ed
hellvita <seredaolga03@gmail.com>
2026-01-15 19:52:15 +0200

    add the Footer component for mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,21 @@
 export default function Footer() {
-  return <footer className="bg-black-900">Footer</footer>;
+  return (
+    <footer className="bg-black-900 py-4 text-center text-white-400 leading-8">
+      <div className="my-0 mx-auto w-80 max-w-full flex flex-col gap-y-5">
+        <p>© 2025 NoteHub. All rights reserved.</p>
+        <div className="flex flex-col gap-y-5">
+          <p>Developer: Olha Sereda</p>
+          <p>
+            Contact us:&#160;
+            <a
+              href="mailto:olhasereda1443@gmail.com"
+              className="hover:text-white-950 hover:underline transition-colors duration-300"
+            >
+              olhasereda1443@gmail.com
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }

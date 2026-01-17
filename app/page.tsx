d69ff14e6bc6e98b2886d64fe89b8b9dc58d0c95
hellvita <seredaolga03@gmail.com>
2026-01-18 01:13:52 +0200

    add styles for the Home page (desktop)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/Home/Hero/Hero";
 import AboutDesktop from "@/components/Home/AboutDesktop/AboutDesktop";
-import About from "@/components/Header/About/About";
+import About from "@/components/Home/About/About";
 
 export default function Home() {
   return (
     <main className="bg-black-800 grow selection:text-purple-800 selection:bg-pink-400">
-      <div className="py-6 mobile:py-25 px-4 flex flex-col justify-center items-center">
+      <div className="py-6 mobile:py-25 px-4 desktop:px-10 flex flex-col desktop:flex-row justify-center items-center desktop:items-baseline">
         <AboutDesktop>
           <Hero />
         </AboutDesktop>
--- components/Home/AboutDesktop/AboutDesktop.tsx
+++ components/Home/AboutDesktop/AboutDesktop.tsx
@@ -5,18 +5,26 @@ interface AboutDesktopProps {
 export default function AboutDesktop({ children }: AboutDesktopProps) {
   return (
     <>
-      <p className="hidden desktop:block">
-        NoteHub is a simple and efficient application designed for managing
-        personal notes. It helps keep your thoughts organized and accessible in
-        one place, whether you are at home or on the go.
-      </p>
+      <div className="flex flex-col gap-24">
+        <div className="w-full h-1"></div>
+        <p className="hidden desktop:block max-w-87 text-s24">
+          NoteHub is a simple and efficient application designed for managing
+          personal notes. It helps keep your thoughts organized and accessible
+          in one place, whether you are at home or on the go.
+        </p>
+      </div>
+
       {children}
-      <p className="hidden desktop:block">
-        The app provides a clean interface for writing, editing, and browsing
-        notes. With support for keyword search and structured organization,
-        NoteHub offers a streamlined experience for anyone who values clarity
-        and productivity.
-      </p>
+
+      <div className="flex flex-col gap-24">
+        <div className="w-full h-1"></div>
+        <p className="hidden desktop:block max-w-87 text-s24 text-right">
+          The app provides a clean interface for writing, editing, and browsing
+          notes. With support for keyword search and structured organization,
+          NoteHub offers a streamlined experience for anyone who values clarity
+          and productivity.
+        </p>
+      </div>
     </>
   );
 }

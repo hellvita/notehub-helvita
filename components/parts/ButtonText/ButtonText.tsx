2f9bfb0257cc2edf3aa59ac714c1d6eda2bb701a
hellvita <seredaolga03@gmail.com>
2026-01-18 19:01:12 +0200

    add motion to the ButtonText component, fix the ButtonTextProps interface and styles for the component

--- components/parts/ButtonText/ButtonText.tsx
+++ components/parts/ButtonText/ButtonText.tsx
@@ -1,39 +1,46 @@
 "use client";
 
-interface ButtonProps {
+import { motion } from "motion/react";
+
+interface ButtonTextProps {
   text: string;
   handler: () => void;
-  paddingX?: string;
-  paddingY?: string;
-  textColor?: string;
+  twStyles?: string;
+  textColorHover?: string;
   bgColorHover?: string;
-  bgColorFocus?: string;
-  borderW?: string;
   borderColorHover?: string;
-  borderColorFocus?: string;
-  fontSize?: string;
-  fontW?: string;
 }
 
 export default function ButtonText({
   text,
   handler,
-  paddingX = "px-5",
-  paddingY = "px-3",
-  textColor = "text-green-200",
-  bgColorHover = "hover:bg-green-200",
-  bgColorFocus = "focus:bg-green-200",
-  borderW = "border",
-  borderColorHover = "hover:border-green-200",
-  borderColorFocus = "focus:border-green-200",
-  fontSize = "text-s16",
-  fontW = "font-normal",
-}: ButtonProps) {
-  const style = `${paddingX} ${paddingY} ${textColor} ${fontW} ${borderW} ${fontSize} ${bgColorHover} ${bgColorFocus} ${borderColorHover} ${borderColorFocus} hover:text-black-900 focus:text-black-900 transition-colors duration-300 cursor-pointer`;
-
+  twStyles = "px-5 text-green-200 text-s16 font-normal border cursor-pointer",
+  textColorHover = "var(--color-black-900)",
+  bgColorHover = "var(--color-green-200)",
+  borderColorHover = "var(--color-green-200)",
+}: ButtonTextProps) {
   return (
-    <button onClick={handler} className={style}>
+    <motion.button
+      onClick={handler}
+      className={twStyles}
+      whileHover={{
+        color: textColorHover,
+        backgroundColor: bgColorHover,
+        borderColor: borderColorHover,
+      }}
+      whileFocus={{
+        color: textColorHover,
+        backgroundColor: bgColorHover,
+        borderColor: borderColorHover,
+      }}
+      whileTap={{
+        color: textColorHover,
+        backgroundColor: bgColorHover,
+        borderColor: borderColorHover,
+      }}
+      transition={{ duration: 0.3 }}
+    >
       {text}
-    </button>
+    </motion.button>
   );
 }

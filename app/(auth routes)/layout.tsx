6379a4081175384141a4e7345f304e27cf33de4c
hellvita <seredaolga03@gmail.com>
2026-01-19 15:28:30 +0200

    add styles for the bg of the auth pages

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -14,5 +14,15 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     router.refresh();
   }, [router]);
 
-  return <div className="grow bg-black-900"> {children} </div>;
+  return (
+    <div
+      className="grow flex flex-col bg-black-900
+     bg-[radial-gradient(#515151_1px,transparent_2px)] 
+     bg-size-[20px_20px]"
+    >
+      <div className="grow bg-linear-to-b from-black-900/90 via-black-900/70 to-black-900/0">
+        {children}
+      </div>
+    </div>
+  );
 }

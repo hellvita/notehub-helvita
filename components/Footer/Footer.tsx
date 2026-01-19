124b9a599eade4bc89fae20fb48a8a84f4e04b49
hellvita <seredaolga03@gmail.com>
2026-01-19 18:25:46 +0200

    update the footer data

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-black-900 py-4 tablet-big:py-9 text-center text-white-400 tablet-big:text-s20 selection:text-blue-400 selection:bg-blue-400-12">
       <div className="flex flex-col gap-y-5 px-5 tablet-big:px-10 tablet-big:flex-row tablet-big:justify-between tablet-big:items-end desktop:items-center desktop:justify-center desktop:gap-x-21">
-        <p>© 2025 NoteHub. All rights reserved.</p>
+        <p>© 2026 NoteHub. All rights reserved.</p>
         <div className="flex flex-col gap-y-5 tablet-big:text-right desktop:text-center desktop:flex-row desktop:gap-x-21">
           <p>Developer: Olha Sereda</p>
           <p>

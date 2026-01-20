88c3ec413c2ed205c93c879141c4e563b84aceca
hellvita <seredaolga03@gmail.com>
2026-01-20 02:32:24 +0200

    add styles for the EditProfile page (tablet/desktop)

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -22,22 +22,34 @@ export default function EditProfilePage() {
 
   return (
     <form className="py-12 px-5 tablet:px-10 bg-black-800 border-t-pink-400 border-t-3">
-      <div className="mb-10 flex gap-10 items-center">
-        <button
-          onClick={handleCancel}
-          className="group tablet:hidden ps-3 py-3 cursor-pointer"
-        >
-          <IconBack className="group-hover:fill-pink-400/88 transition-colors duration-300 mobile:text-s20" />
-        </button>
-        <h1 className="mobile:text-s32 tablet:text-s40 desktop:text-s56 font-medium">
-          Edit Page
-        </h1>
+      <div className="mb-10 flex items-center tablet:justify-between">
+        <div className="flex gap-10 items-center ">
+          <button
+            onClick={handleCancel}
+            type="button"
+            className="group p-3 cursor-pointer"
+          >
+            <IconBack className="group-hover:fill-pink-400/88 transition-colors duration-300 text-s20 tablet:text-s28" />
+          </button>
+          <h1 className="mobile:text-s32 tablet:text-s40 desktop:text-s56 font-medium">
+            Edit Page
+          </h1>
+        </div>
+
+        <ButtonText
+          type="submit"
+          text="Save"
+          handler={() => {}}
+          twStyles="py-2 px-3 max-w-22 max-h-11.5 text-pink-400 text-s28 font-medium border cursor-pointer max-tablet:hidden"
+          bgColorHover="var(--color-pink-400)"
+          borderColorHover="var(--color-pink-400)"
+        />
       </div>
 
-      <div className="flex flex-col gap-10 items-center tablet-big:flex-row">
+      <div className="flex flex-col gap-10 items-center tablet:flex-row">
         <DefaultAvatar />
 
-        <div className="w-full flex flex-col gap-y-5 tablet:max-tablet-big:flex-row tablet:max-tablet-big:justify-between mb-13 tablet:mb-0">
+        <div className="w-full flex flex-col gap-y-5 tablet:max-tablet:flex-row tablet:max-tablet-big:justify-between mb-13 tablet:mb-0">
           <FormInput
             label="Username"
             id="username"
@@ -45,10 +57,14 @@ export default function EditProfilePage() {
             name="username"
             defaultValue="my-user-name"
             hint="Username"
-            twStylesLabel="text-yellow-500 mobile:text-s24"
+            twStylesLabel="text-pink-400 mobile:text-s24"
             twStylesInput="py-2 px-4 outline-0 mobile:text-s28 placeholder:font-light placeholder:text-white-400/50"
           />
-          <UserInfo label="Email" value={normalizedEmail} />
+          <UserInfo
+            label="Email"
+            value={normalizedEmail}
+            twStylesLabel="text-pink-400 mobile:text-s24"
+          />
         </div>
       </div>
 
@@ -56,7 +72,7 @@ export default function EditProfilePage() {
         type="submit"
         text="Save"
         handler={() => {}}
-        twStyles="p-3.5 min-w-0 my-0 mx-auto block w-full tablet:max-w-58 text-pink-400 mobile:text-s20 tablet-big:text-s24 font-medium border cursor-pointer"
+        twStyles="p-3.5 min-w-0 my-0 mx-auto block w-full text-pink-400 mobile:text-s20 font-medium border cursor-pointer tablet:hidden"
         bgColorHover="var(--color-pink-400)"
         borderColorHover="var(--color-pink-400)"
       />

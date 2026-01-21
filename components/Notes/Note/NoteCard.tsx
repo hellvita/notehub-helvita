d37724cdd06509db3d33784ba9b4ebe504a3ff5b
hellvita <seredaolga03@gmail.com>
2026-01-21 23:10:01 +0200

    add styles for the ButtonLink component

--- components/Notes/Note/NoteCard.tsx
+++ components/Notes/Note/NoteCard.tsx
@@ -12,7 +12,7 @@ export default function NoteCard({ note }: NoteProps) {
   // ** TODO sliceContent()
 
   return (
-    <div className="relative w-full max-w-93 p-6 bg-black-800">
+    <div className="relative w-full max-w-93 p-6 bg-black-800 selection:text-blue-400 selection:bg-blue-400-12">
       <CloseButton />
       <h2 className="mobile:text-s20 font-medium leading-8 mb-3">
         {note.title}
@@ -21,7 +21,10 @@ export default function NoteCard({ note }: NoteProps) {
         {note.content}
       </p>
       <div className="flex flex-col-reverse gap-y-5 tablet:flex-row tablet:gap-x-4 items-center">
-        <ButtonLink text="View details" />
+        <ButtonLink
+          text="View details"
+          twStyles="py-2.5 px-5 w-full tablet:min-w-31.75 text-center text-green-200 mobile:text-s16 leading-5 border cursor-pointer selection:text-purple-800 selection:bg-white-950/90"
+        />
         <div className="flex justify-between items-center w-full">
           <EditButton />
           <TagLabel tagName={note.tag} />

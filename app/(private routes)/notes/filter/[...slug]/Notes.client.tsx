c4267a9e1cedbc7eff3f4b2bcb06e5f748e3a922
hellvita <seredaolga03@gmail.com>
2026-01-21 16:08:15 +0200

    add the CreateNote button

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -1,4 +1,5 @@
 import SearchBar from "@/components/Notes/SearchBar/SearchBar";
+import ButtonLink from "@/components/parts/ButtonLink/ButtonLink";
 import { NoteTag } from "@/types/note";
 
 interface NotesClientProps {
@@ -8,7 +9,16 @@ interface NotesClientProps {
 export default function NotesClient({ currentTag }: NotesClientProps) {
   return (
     <div>
-      <SearchBar />
+      <div className="mb-5 flex items-center justify-center gap-x-7">
+        <SearchBar />
+        <ButtonLink
+          text="Create note +"
+          twStyles="px-6 py-4 text-blue-400 text-s24 font-medium border cursor-pointer max-tablet-big:hidden"
+          bgColorHover="var(--color-blue-400)"
+          borderColorHover="var(--color-blue-400)"
+        />
+      </div>
+
       <p>Notes</p>
     </div>
   );
--- components/Notes/SearchBar/SearchBar.tsx
+++ components/Notes/SearchBar/SearchBar.tsx
@@ -12,7 +12,7 @@ export default function SearchBar() {
     <motion.input
       type="text"
       placeholder="Search notes"
-      className="mb-5 border border-white-950/10 bg-white-950/2 w-full tablet-big:max-w-2xl p-4 mobile:text-s20 placeholder:text-white-400/50 text-white-950 outline-0"
+      className="border border-white-950/10 bg-white-950/2 w-full tablet-big:max-w-2xl p-4 mobile:text-s20 placeholder:text-white-400/50 text-white-950 outline-0"
       whileFocus={{ borderColor: "var(--color-blue-400)" }}
       whileTap={{ borderColor: "var(--color-blue-400)" }}
     />

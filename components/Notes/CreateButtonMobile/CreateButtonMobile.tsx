69f430edb91eef4d1c0a88193d263c72896d94c8
hellvita <seredaolga03@gmail.com>
2026-01-23 01:15:04 +0200

    fix styles for the CreateButtonMobile component

--- components/Notes/CreateButtonMobile/CreateButtonMobile.tsx
+++ components/Notes/CreateButtonMobile/CreateButtonMobile.tsx
@@ -5,7 +5,7 @@ export default function CreateButtonMobile() {
     <div className="sticky flex justify-end bottom-8 tablet:bottom-10 tablet-big:hidden pointer-events-none">
       <ButtonLink
         text="+"
-        twStyles="border rounded-[50%] mobile:text-s40 font-light text-blue-400 bg-white-950/10 px-4 py-2.25 pointer-events-auto"
+        twStyles="border rounded-[50%] mobile:text-s32 font-light text-blue-400/80 bg-black-900/90 px-4 py-2.25 pointer-events-auto"
         bgColorHover="var(--color-blue-400)"
         borderColorHover="var(--color-blue-400)"
       />

c57b8a04a70b526afb1cc9e16e727456097f4eaa
hellvita <seredaolga03@gmail.com>
2026-01-22 22:39:38 +0200

    change the note card styles

--- components/Notes/Note/NoteCard.tsx
+++ components/Notes/Note/NoteCard.tsx
@@ -11,7 +11,7 @@ interface NoteProps {
 
 export default function NoteCard({ note }: NoteProps) {
   return (
-    <li className="group/card relative w-full max-w-93 p-6 bg-black-800 selection:text-blue-400 selection:bg-blue-400-12">
+    <li className="relative w-full max-w-93 p-6 bg-black-800 selection:text-blue-400 selection:bg-blue-400-12">
       <DeleteButton />
       <h2 className="mobile:text-s20 font-medium leading-8 mb-3">
         {note.title}
@@ -25,8 +25,8 @@ export default function NoteCard({ note }: NoteProps) {
           twStyles="py-2.5 px-5 w-full tablet:min-w-31.75 text-center text-green-200 mobile:text-s16 leading-5 border cursor-pointer selection:text-purple-800 selection:bg-white-950/90"
         />
         <div className="flex justify-between items-center w-full">
-          <EditButton />
           <TagLabel tagName={note.tag} />
+          <EditButton />
         </div>
       </div>
     </li>

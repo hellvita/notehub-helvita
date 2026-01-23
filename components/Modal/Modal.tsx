3d2e9c72c033fcb344546b5d64850922fe50443d
hellvita <seredaolga03@gmail.com>
2026-01-23 02:08:41 +0200

    add the Modal component (without styles)

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { body } from "motion/react-client";
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ onClose, children }: ModalProps) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) onClose();
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div onClick={handleBackdropClick}>{children}</div>,
+    document.body,
+  );
+}

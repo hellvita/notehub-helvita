e99fa6619a6da519db5574235e07b232486c3816
hellvita <seredaolga03@gmail.com>
2026-01-25 01:27:33 +0200

    add the baner image for the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     url: baseURL,
     images: [
       {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        url: `${baseURL}/notehub-helvita-og-meta.jpg`,
         width: 1200,
         height: 630,
         alt: "NoteHub",

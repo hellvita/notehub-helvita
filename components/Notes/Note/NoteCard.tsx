9ee7351fa222d75393a2189a3cd3b716c38e9862
hellvita <seredaolga03@gmail.com>
2026-01-25 04:12:17 +0200

    fix styles for the NoteCard component

--- components/Notes/Note/NoteCard.tsx
+++ components/Notes/Note/NoteCard.tsx
@@ -11,23 +11,25 @@ interface NoteProps {
 
 export default function NoteCard({ note }: NoteProps) {
   return (
-    <li className="relative w-full max-w-93 p-6 bg-black-800 selection:text-blue-400 selection:bg-blue-400-12">
+    <li className="relative flex flex-col w-full max-w-93 p-6 bg-black-800 selection:text-blue-400 selection:bg-blue-400-12">
       <DeleteButton />
       <h2 className="mobile:text-s20 font-medium leading-8 mb-3">
         {note.title}
       </h2>
-      <p className="mobile:text-s16 text-white-400 leading-5 mb-5">
+      <p className="grow mobile:text-s16 text-white-400 leading-5 mb-5">
         {sliceContent(note.content)}
       </p>
-      <div className="flex flex-col-reverse gap-y-5 tablet:flex-row tablet:gap-x-4 items-center">
-        <ButtonLink
-          text="View details"
-          href={`/notes/${note.id}`}
-          twStyles="py-2.5 px-5 w-full tablet:min-w-31.75 text-center text-green-200 mobile:text-s16 leading-5 border cursor-pointer selection:text-purple-800 selection:bg-white-950/90"
-        />
-        <div className="flex justify-between items-center w-full">
-          <TagLabel tagName={note.tag} />
-          <EditButton noteId={note.id} />
+      <div>
+        <div className="flex flex-col-reverse gap-y-5 tablet:flex-row tablet:gap-x-4 items-center">
+          <ButtonLink
+            text="View details"
+            href={`/notes/${note.id}`}
+            twStyles="py-2.5 px-5 w-full tablet:min-w-31.75 text-center text-green-200 mobile:text-s16 leading-5 border cursor-pointer selection:text-purple-800 selection:bg-white-950/90"
+          />
+          <div className="flex justify-between items-center w-full">
+            <TagLabel tagName={note.tag} />
+            <EditButton noteId={note.id} />
+          </div>
         </div>
       </div>
     </li>

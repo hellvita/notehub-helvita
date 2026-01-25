242d801d6b5886c4a2380ce538c3d93ecbc2aac8
hellvita <seredaolga03@gmail.com>
2026-01-25 16:43:03 +0200

    add the metadata for the CreateNote page

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -1,4 +1,25 @@
+import type { Metadata } from "next";
 import CreateNoteClient from "./CreateNote.client";
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+export const metadata: Metadata = {
+  title: "Add new note",
+  description: "You can easily create your new note here.",
+  openGraph: {
+    title: "Add new note",
+    description: "You can easily create your new note here.",
+    url: `${baseURL}/notes/action/create`,
+    images: [
+      {
+        url: `${baseURL}/notehub-helvita-og-meta.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Add new note",
+      },
+    ],
+  },
+};
 
 export default function CreateNote() {
   return <CreateNoteClient />;

9b467b800e85b7d4b9b18114f4b85c377f860aae
hellvita <seredaolga03@gmail.com>
2026-01-25 02:10:45 +0200

    add handler for the Register page

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -1,12 +1,41 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { register, RegisterRequest } from "@/lib/api/clientApi";
+import { useAuthStore } from "@/lib/store/authStore";
 import AuthForm from "@/components/Auth/AuthForm";
 import FormInput from "@/components/parts/FormInput/FormInput";
 import ButtonText from "@/components/parts/ButtonText/ButtonText";
 
 export default function RegisterPage() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      setError("");
+
+      const formValues = Object.fromEntries(
+        formData,
+      ) as unknown as RegisterRequest;
+
+      const res = await register(formValues);
+
+      if (res) {
+        setUser(res);
+        router.push("/profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError((error as Error).message ?? "Oops... some error");
+    }
+  };
+
   return (
-    <AuthForm handleSubmit={() => {}} title="Sign Up">
+    <AuthForm handleSubmit={handleSubmit} title="Sign Up">
       <div className="flex flex-col gap-y-9 tablet:gap-y-8 mb-14 tablet:mb-13">
         <FormInput
           label="Email"

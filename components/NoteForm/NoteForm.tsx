6dcd2b7c4f7873439f4753040cd79fe7b9ae3b78
hellvita <seredaolga03@gmail.com>
2026-01-25 19:59:13 +0200

    change styles for the mobile note form component

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -55,7 +55,7 @@ export default function NoteForm({
             minLength={3}
             maxLength={50}
             onChange={handleChange}
-            className="text-center tablet-big:text-left w-[84%] tablet-big:w-full mobile:text-s28 tablet-big:text-s24 font-medium placeholder:text-white-950/50 resize-none field-sizing-content overflow-hidden outline-none selection:text-blue-400 selection:bg-blue-400-12"
+            className="text-center tablet-big:text-left w-[80%] tablet:w-[84%] tablet-big:w-full mobile:text-s28 tablet-big:text-s24 font-medium placeholder:text-white-950/50 resize-none field-sizing-content overflow-hidden outline-none selection:text-blue-400 selection:bg-blue-400-12"
           ></textarea>
         </div>
 
@@ -67,7 +67,7 @@ export default function NoteForm({
           rows={3}
           maxLength={500}
           onChange={handleChange}
-          className="w-full mobile:leading-7 mobile:text-s20 text-white-400 placeholder:text-white-400/50 resize-none field-sizing-content overflow-hidden outline-none whitespace-pre-wrap mb-8 selection:text-blue-400 selection:bg-blue-400-12"
+          className="w-full mobile:leading-7 mobile:text-s20 text-white-400 placeholder:text-white-400/50 resize-none field-sizing-content overflow-hidden outline-none whitespace-pre-wrap mb-10 tablet:mb-8 selection:text-blue-400 selection:bg-blue-400-12"
         ></textarea>
 
         <TagList
@@ -76,7 +76,7 @@ export default function NoteForm({
           defaultTag={note ? note.tag : draft ? draft.tag : undefined}
           handleChange={handleChange}
           twStylesContainer="mb-8 tablet-big:mb-0 flex flex-wrap items-center justify-center gap-x-2 tablet:gap-x-5 gap-y-3 tablet:gap-y-6"
-          twStylesItem="mobile:text-s12 px-4 py-0.5"
+          twStylesItem="text-s20 tablet:text-s12 px-4 py-0.5"
         />
 
         <ButtonText

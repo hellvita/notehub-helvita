bf36bf8a8e740f1b890022c18b1c083ff2b35390
hellvita <seredaolga03@gmail.com>
2026-02-13 16:42:07 +0200

    update the proxy (make observe only)

--- proxy.ts
+++ proxy.ts
@@ -1,69 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { parse } from "cookie";
-import { checkSession } from "./lib/api/serverApi";
 
 const privateRoutes = ["/profile", "/notes"];
 const publicRoutes = ["/sign-in", "/sign-up"];
 
 export async function proxy(request: NextRequest) {
-  const cookieStore = await cookies();
-
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
-
   const { pathname } = request.nextUrl;
+  const accessToken = request.cookies.get("accessToken");
 
-  const isPublicRoute = publicRoutes.some((route) =>
-    pathname.startsWith(route),
-  );
-  const isPrivateRoute = privateRoutes.some((route) =>
-    pathname.startsWith(route),
-  );
+  const isPublicRoute = publicRoutes.some((r) => pathname.startsWith(r));
+  const isPrivateRoute = privateRoutes.some((r) => pathname.startsWith(r));
 
-  if (!accessToken) {
-    if (refreshToken) {
-      const data = await checkSession();
-      const setCookie = data.headers["set-cookie"];
+  if (!accessToken && isPrivateRoute) {
+    const loginUrl = new URL("/sign-in", request.url);
 
-      if (setCookie) {
-        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+    loginUrl.searchParams.set("redirect", pathname);
 
-        for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-
-          const options = {
-            ...(parsed.Expires && { expires: new Date(parsed.Expires) }),
-            ...(parsed.Path && { path: parsed.Path }),
-            ...(parsed["Max-Age"] && { maxAge: Number(parsed["Max-Age"]) }),
-          };
-
-          if (parsed.accessToken)
-            cookieStore.set("accessToken", parsed.accessToken, options);
-
-          if (parsed.refreshToken)
-            cookieStore.set("refreshToken", parsed.refreshToken, options);
-        }
-
-        if (isPrivateRoute)
-          return NextResponse.redirect(request.nextUrl, {
-            headers: cookieArray.map((cookie) => ["Set-Cookie", cookie]),
-          });
-
-        return NextResponse.redirect(new URL("/", request.url), {
-          headers: { Cookie: cookieStore.toString() },
-        });
-      }
-    }
-
-    if (isPublicRoute) return NextResponse.next();
-
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(loginUrl);
   }
 
-  if (isPrivateRoute) return NextResponse.next();
+  if (accessToken && isPublicRoute) {
+    const homeUrl = new URL("/", request.url);
 
-  return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(homeUrl);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {

6e8a40ebd6e8ed91af52b7229bdaeaf6c49da0f3
hellvita <seredaolga03@gmail.com>
2026-02-14 23:07:40 +0200

    add the bff method for avatar updating

---
+++ app/api/users/me/avatar/route.ts
@@ -0,0 +1,37 @@
+export const dynamic = "force-dynamic";
+
+import { NextResponse } from "next/server";
+import { api } from "../../../api";
+import { cookies } from "next/headers";
+import { logErrorResponse } from "../../../_utils/utils";
+import { isAxiosError } from "axios";
+
+export async function PATCH(request: Request) {
+  try {
+    const cookieStore = await cookies();
+
+    const formData = await request.formData();
+
+    const res = await api.patch("/users/me/avatar", formData, {
+      headers: {
+        Cookie: cookieStore.toString(),
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status },
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}

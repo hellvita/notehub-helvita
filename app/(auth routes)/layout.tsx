f09d9aa223649a260ba0a764bb5cd47ab04ae509
hellvita <seredaolga03@gmail.com>
2026-02-18 17:14:37 +0200

    add loading state for the public (auth) routes

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import BgDot from "@/components/parts/BgDot/BgDot";
+import Loader from "@/components/Loader/Loader";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -10,15 +11,22 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
-    router.refresh();
+    startTransition(() => {
+      router.refresh();
+      setIsMounted(true);
+    });
   }, [router]);
+
+  const isLoading = isPending || !isMounted;
 
   return (
     <BgDot>
       <div className="min-w-0 my-0 mx-auto mobile:max-w-82 tablet:max-w-150 tablet-big:max-w-177 mobile:leading-none selection:text-purple-800 selection:bg-pink-400">
-        {children}
+        {isLoading ? <Loader /> : children}
       </div>
     </BgDot>
   );

6aed6f3b211da61eba7d14cffd264c8a766861e1
hellvita <seredaolga03@gmail.com>
2026-02-20 19:01:13 +0200

    remove the toReversed method for the notes list

--- components/Notes/NoteList/NoteList.tsx
+++ components/Notes/NoteList/NoteList.tsx
@@ -62,7 +62,7 @@ export default function NoteList({ notes }: NoteListProps) {
   return (
     <>
       <ul className="grid justify-center grid-cols-[repeat(100%,1fr)] grid-sm:grid-cols-[repeat(auto-fill,372px)] gap-y-5 tablet:gap-8 tablet:max-w-315 mb-10">
-        {notes.toReversed().map((note) => (
+        {notes.map((note) => (
           <NoteCard
             note={note}
             key={note.id}
